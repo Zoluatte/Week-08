21559d419ea5e5f80fd3e2d0882aa12c6292b2d0
phruksa4165@outlook.com <phruksa4165@outlook.com>
2019-11-08 21:47:31 +0700

    Commit

--- 61030035/lab08/lab08/Properties/AssemblyInfo.cs
+++ 61030035/lab08/lab08/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("lab8")]
+[assembly: AssemblyTitle("lab08")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("lab8")]
+[assembly: AssemblyProduct("lab08")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("756cea61-62e5-4a45-8903-f74618ab2c84")]
+[assembly: Guid("4ab943e5-2cdc-4654-a0a9-710b36bdac84")]
 
 // Version information for an assembly consists of the following four values:
 //
