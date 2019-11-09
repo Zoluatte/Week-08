0a697d55c884c12cedd0a6754e5789a9c61a237c
phruksa4165@outlook.com <phruksa4165@outlook.com>
2019-11-09 11:26:59 +0700

    COMMIT

--- 61030035/lab08/lab08/Program.cs
+++ 61030035/lab08/lab08/Program.cs
@@ -10,7 +10,17 @@ namespace lab08
     {
         static void Main(string[] args)
         {
-           
+            float[] GPS = new float[8];
+            for(int n=0;n<GPS.Length;n++)
+            {
+                Console.Write($"GPS TREM{n + 1} : ");
+                GPS[n] = float.Parse(Console.ReadLine());
+            }
+            foreach(float j in GPS)
+            Console.Write($"{j,0:N}");
+            Console.WriteLine();
+            Console.WriteLine("GPA is {0,0:N}", GPS.Sum() / GPS.Length);
+            Console.ReadLine();
         }
     }
 }
