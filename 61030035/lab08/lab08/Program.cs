c85765dfde11faf2fa9f0eca7fe3304af79c5436
phruksa4165@outlook.com <phruksa4165@outlook.com>
2019-11-13 04:18:10 +0700

    commit week8

--- 61030035/lab08/lab08/Program.cs
+++ 61030035/lab08/lab08/Program.cs
@@ -11,46 +11,57 @@ namespace lab08
     {
         static void Main(string[] args)
         {
-            Hashtable north = new Hashtable();
-            north.Add("50000", "CHIANGMAI");
-            north.Add("20000", "LAMPANG");
-            north.Add("64000", "SUKHOTHAI");
-            north.Add("61000", "UTHAITHANI");
-            north.Add("55000", "NAN");
-            north.Add("65000", "PHITSANULOK");
-            north.Add("54000", "PHRAE");
-            north.Add("58000", "MAEHONGSON");
-            north.Add("57000", "CHIANGRAI");
-            north.Add("63000", "TAK");
+            Hashtable student = new Hashtable();
+            student.Add("61030001", "KURNGTHAI SUMPRUNG");
+            student.Add("61030006", "KHANITA VIRIYADECHOCHAI");
+            student.Add("61030007", "KHOMKRIT DEANGNUAN");
+            student.Add("61030008", "JANISTAR PIMPATHONG");
+            student.Add("61030009", "JANJIRA INTARASUK");
+            student.Add("61030017", "NAPAKORN SUKKASEM");
+            student.Add("61030018", "NAPAT KUMSEN");
+            student.Add("61030020", "NATDANAI IAMSAAD");
+            student.Add("61030021", "NATTAPAT SRISANAM");
+            student.Add("61030031", "NAKORN SRIKASEM");
+            student.Add("61030035", "PHRUKSA KAEWMANEE");
+            student.Add("61030040", "PEERAPON SAYYOD");
+            student.Add("61030045", "MAETHAPHON CHANGART");
+            student.Add("61030050", "WATSAPHON KHUNTHORN");
+            student.Add("61030054", "SUPAKHON MAPACHUM");
+            student.Add("61030055", "SUPHAPHIT SUKA");
+            student.Add("61030057", "SAKULRAT PLUKSAKUL");
+            student.Add("61030059", "SIRADA SOONTHORN");
+            student.Add("61030060", "SUPHON SAEJIA");
+            student.Add("61030234", "CHONELADA PHONDEE");
+            student.Add("61030238", "NARATIP SANGJANDA");
+            student.Add("61030242", "WARINRAMPAI KHUNPARKDEE");
+            student.Add("61030243", "WIDSAWA DHAMCHOMSAP");
 
-            foreach (DictionaryEntry pcn in north)
+
+            foreach (DictionaryEntry com in student)
                {
                 
-                    Console.WriteLine($"{pcn.Key} => {pcn.Value}");
+                    Console.WriteLine($"{com.Key} => {com.Value}");
             }
             Console.WriteLine();
 
-            Console.WriteLine("Enter PostCode North :");
-            string n = Console.ReadLine().ToUpper ();
+            Console.WriteLine("Enter PostCode Student :");
+            string s = Console.ReadLine().ToUpper ();
                
 
-            foreach(DictionaryEntry pnc in north)
+            foreach(DictionaryEntry com in student)
             {
-                if (pnc.Equals(pnc.Key))
-                    Console.WriteLine($"{pnc.Key} => {pnc.Value }");
-                       
-                   
-
+                if(s.Equals(com.Key))
+                Console.WriteLine("{0}", com.Value);
             }
-         
-
-
-            Console.ReadLine();
+                   Console.ReadLine();
 
             }
         
     }
 }
+
+
+  
                 
 
     
