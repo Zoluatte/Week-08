4d6672e23194f8ea4c62a7df57c293e61e89eb62
kkkanitar <khanita03272@gmail.com>
2019-11-27 21:51:42 +0700

    commit3

--- 61030006/Week-08/Week-08/Program.cs
+++ 61030006/Week-08/Week-08/Program.cs
@@ -10,29 +10,60 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
-            int[] data = new int[10];
-            data[0] = 0;
-            data[1] = 1;
-            data[2] = 2;
-            data[3] = 3;
-            data[4] = 4;
-            data[5] = 5;
-            data[6] = 6;
-            data[7] = 7;
-            data[8] = 8;
-            data[9] = 9;
+            float[] durian = new float[25];
+            durian[0] = 0;
+            durian[1] = 1;
+            durian[2] = 2;
+            durian[3] = 3;
+            durian[4] = 4;
+            durian[5] = 5;
+            durian[6] = 6;
+            durian[7] = 7;
+            durian[8] = 8;
+            durian[9] = 9;
+            durian[10] = 10;
+            durian[11] = 11;
+            durian[12] = 12;
+            durian[13] = 13;
+            durian[14] = 14;
+            durian[15] = 15;
+            durian[16] = 16;
+            durian[17] = 17;
+            durian[18] = 18;
+            durian[19] = 19;
+            durian[20] = 20;
+            durian[21] = 21;
+            durian[22] = 22;
+            durian[23] = 23;
+            durian[24] = 24;
+
+            Console.WriteLine(durian[0]);
+            Console.WriteLine(durian[1]);
+            Console.WriteLine(durian[2]);
+            Console.WriteLine(durian[3]);
+            Console.WriteLine(durian[4]);
+            Console.WriteLine(durian[5]);
+            Console.WriteLine(durian[6]);
+            Console.WriteLine(durian[7]);
+            Console.WriteLine(durian[8]);
+            Console.WriteLine(durian[9]);
+            Console.WriteLine(durian[10]);
+            Console.WriteLine(durian[11]);
+            Console.WriteLine(durian[12]);
+            Console.WriteLine(durian[13]);
+            Console.WriteLine(durian[14]);
+            Console.WriteLine(durian[15]);
+            Console.WriteLine(durian[16]);
+            Console.WriteLine(durian[17]);
+            Console.WriteLine(durian[18]);
+            Console.WriteLine(durian[19]);
+            Console.WriteLine(durian[20]);
+            Console.WriteLine(durian[21]);
+            Console.WriteLine(durian[22]);
+            Console.WriteLine(durian[23]);
+            Console.WriteLine(durian[24]);
             
-            Console.WriteLine(data[0]);
-            Console.WriteLine(data[1]);
-            Console.WriteLine(data[2]);
-            Console.WriteLine(data[3]);
-            Console.WriteLine(data[4]);
-            Console.WriteLine(data[5]);
-            Console.WriteLine(data[6]);
-            Console.WriteLine(data[7]);
-            Console.WriteLine(data[8]);
-            Console.WriteLine(data[9]);
-            
+
             Console.ReadLine();
 
         }
