7e1b88570a81cc26958782a82eafce0f672c3f9e
kkkanitar <khanita03272@gmail.com>
2019-11-28 00:24:17 +0700

    commit23

--- 61030006/Week-08/Week-08/Program.cs
+++ 61030006/Week-08/Week-08/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Diagnostics;
 
 namespace Week_08
 {
@@ -11,29 +12,37 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
+            Hashtable weeks = new Hashtable();
+            weeks.Add("1", "Sunday");
+            weeks.Add("2", "Monday");
+            weeks.Add("3", "Tuesday");
+            weeks.Add("4", "Wednesday");
+            weeks.Add("5", "Thursday");
+            weeks.Add("6", "Friday");
+            weeks.Add("7", "Saturday");
 
-            Dictionary<string, int> dict1 = new Dictionary<string, int>();
-            dict1.Add("One", 1);
-            dict1.Add("Two", 2);
-            dict1.Add("Three", 3);
-            dict1.Add("Four", 4);
-            dict1.Add("Five", 5);
-            dict1.Add("Six", 6);
-            dict1.Add("Seven", 7);
-            dict1.Add("Eight", 8);
-            dict1.Add("Nine", 9);
-            dict1.Add("Ten", 10);
-            if (dict1.ContainsKey("One"))
+            Console.WriteLine("---- elements in Hashtable weeks ----");
+            foreach (DictionaryEntry day in weeks)
             {
-                int value = dict1["One"];
-                Console.WriteLine(value);
+                Console.WriteLine(day.Key + "   -   " + day.Value);
             }
-            if (dict1.ContainsKey("Eleven"))
+
+            Console.WriteLine("\n---- weeks.Remove(\"4\"); ----");
+            weeks.Remove("4");
+            foreach (DictionaryEntry day in weeks)
             {
-                int value = dict1["Eleven"];
-                Console.WriteLine(value);
+                Console.WriteLine(day.Key + "   -   " + day.Value);
             }
+
+            Console.WriteLine("\n---- weeks.Clear(); ----");
+            weeks.Clear();
+            foreach (DictionaryEntry day in weeks)
+            {
+                Console.WriteLine(day.Key + "   -   " + day.Value);
+            }
+
             Console.ReadLine();
         }
+    
     }
 }
