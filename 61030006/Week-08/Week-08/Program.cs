93fef0d79f43c00fa32064ba1560adf54d795b10
kkkanitar <khanita03272@gmail.com>
2019-11-27 23:52:21 +0700

    commit17

--- 61030006/Week-08/Week-08/Program.cs
+++ 61030006/Week-08/Week-08/Program.cs
@@ -10,19 +10,7 @@ namespace Week_08
     {
         static void Main(string[] args)
         {
-            int[,] multiplyTable = new int[12, 12];
-            for (int row = 0; row < multiplyTable.GetLength(0); row++)
-            {
-                for (int col = 0, mul = 2; col < multiplyTable.GetLength(1); col++, mul++)
-                {
-                    if (mul == 13)
-                        continue;
-                    Console.Write("{0 , 3} ", mul * (row + 1));
-                    //Console.Write($"{row},{col} ");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
+           
         }
     }
 }
