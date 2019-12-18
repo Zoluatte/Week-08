1e7252157c6477bbca2490ace86fbf7caebbb41c
Zoluatte <53931447+Zoluatte@users.noreply.github.com>
2019-12-18 18:23:54 +0700

    by napat kumsen

---
+++ 61030018/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sname = null;
+            string sid = null;
+            int id;
+
+            Hashtable student = new Hashtable();
+
+
+            StreamReader fname = null;
+            StreamReader fid = null;
+
+            try
+            {
+                fname = new StreamReader("./name.txt");//file form folder bin
+                fid = new StreamReader("./id.txt");
+                while (((sname = fname.ReadLine().ToString()) != ".") && ((sid = fid.ReadLine().ToString()) != "."))
+                {
+                    student.Add(sid, sname);
+                    Console.WriteLine($"{sname} => {sid}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine();
+        insert:
+            try
+            {
+                Console.Write("Enter Postcode : ");
+                id = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                goto insert;
+            }
+            Console.WriteLine();
+            foreach (DictionaryEntry dicstudent in student)
+            {
+                if (dicstudent.Key.Equals(id.ToString()))
+                    Console.WriteLine(dicstudent.Key + "    " + dicstudent.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
